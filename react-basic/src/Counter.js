201952db0bee20a62e03319534ebdf4033ec688e
jjy321501 <jjy321501@naver.com>
2022-07-31 23:35:08 +0900

    useState 학습

---
+++ react-basic/src/Counter.js
@@ -0,0 +1,41 @@
+import MyHeader from "./MyHeader";
+import React, { useState } from "react";
+
+const Counter = () => {
+  //0에서 출발
+  //1씩 증가
+  //1씩 감소
+  //count 상태
+  const [count, setCount] = useState(0);
+
+  const onIncrease = () => {
+    setCount(count + 1);
+  };
+
+  const onDecrease = () => {
+    setCount(count - 1);
+  };
+  const [count2, setCount2] = useState(0);
+
+  const onIncrease2 = () => {
+    setCount2(count2 + 1);
+  };
+
+  const onDecrease2 = () => {
+    setCount2(count2 - 1);
+  };
+
+  return (
+    <div>
+      <h2>{count}</h2>
+      <button onClick={onIncrease}>+</button>
+      <button onClick={onDecrease}>-</button>
+
+      <h2>{count2}</h2>
+      <button onClick={onIncrease2}>+</button>
+      <button onClick={onDecrease2}>-</button>
+    </div>
+  );
+};
+
+export default Counter;
