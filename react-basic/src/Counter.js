410d2f1dc119bbc7be264eba7d558ffe783d9ad8
jjy321501 <jjy321501@naver.com>
2022-08-01 02:26:14 +0900

    props  학습

--- react-basic/src/Counter.js
+++ react-basic/src/Counter.js
@@ -1,12 +1,13 @@
 import MyHeader from "./MyHeader";
 import React, { useState } from "react";
+import OddEvenResult from "./OddEvenResult";
 
-const Counter = () => {
+const Counter = ({ initialValue }) => {
   //0에서 출발
   //1씩 증가
   //1씩 감소
   //count 상태
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(initialValue);
 
   const onIncrease = () => {
     setCount(count + 1);
@@ -15,27 +16,19 @@ const Counter = () => {
   const onDecrease = () => {
     setCount(count - 1);
   };
-  const [count2, setCount2] = useState(0);
-
-  const onIncrease2 = () => {
-    setCount2(count2 + 1);
-  };
-
-  const onDecrease2 = () => {
-    setCount2(count2 - 1);
-  };
 
   return (
     <div>
       <h2>{count}</h2>
       <button onClick={onIncrease}>+</button>
       <button onClick={onDecrease}>-</button>
-
-      <h2>{count2}</h2>
-      <button onClick={onIncrease2}>+</button>
-      <button onClick={onDecrease2}>-</button>
+      <OddEvenResult count={count} />
     </div>
   );
+};
+
+Counter.defaultProps = {
+  initialValue: 0,
 };
 
 export default Counter;
